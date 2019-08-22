060967c06ed8c3595549df59fc4cb351a0350f8d
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-08-22 17:06:34 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,11 +1,11 @@
 class Show < ActiveRecord::Base 
   
-  def highest_rating
+  def ::highest_rating
     Show.maximum(:rating)
   end
   
   def most_popular_show
-    Show.where()
+    Show.where("ratingg")
   end
   
 end 
