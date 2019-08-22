c5e414d7da1e377e7c6992f55c69984c17386e4f
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-08-22 16:49:21 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -4,7 +4,7 @@ class CreateShows < ActiveRecord::Migrate[5.2]
       t.string :name 
       t.string :network 
       t.string :day 
-      t.float :rating 
+      t.integer :rating 
     end
   end
 end
